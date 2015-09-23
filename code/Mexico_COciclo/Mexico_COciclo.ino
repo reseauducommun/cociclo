9dc7bbda63da1f4b8567b2024d7d49c84a649a34
Alexandre Castonguay <acastonguay@artengine.ca>
2015-09-23 14:40:39 -0500

    yo limpio

--- code/Mexico_COciclo/Mexico_COciclo.ino
+++ code/Mexico_COciclo/Mexico_COciclo.ino
@@ -2,33 +2,48 @@ int CO = 0;
 int CO2 = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+
 Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-CO = (analogRead(A7));
+CO = (analogRead(A7)); leer el valor del sensor
+Serial.print("CO : ");
 Serial.println(CO);
-CO2 = map(CO,100, 400, 150,666); // remap de l'ancienne valeur à quelque chose d'audible
-Serial.println(CO2); //nuevo, le CO2 est une chistosa
 
-if (CO2 < 250)
+CO2 = map(CO,80, 400, 150,666); // sustituir los valores en un nuevo intervalo para escuchar
+Serial.print("CO2 : ");
+Serial.println(CO2); //nuevo, 'CO2' es une chistosa
+Serial.println();
+
+////////////////////////////////////////////////////////////////////////
+//                     Para la version Android                        //
+////////////////////////////////////////////////////////////////////////
+
+tone(5,CO2,400); // sortida (PWM 5 en este caso), variable, duración del pulse
+delay(100);
+
+
+////////////////////////////////////////////////////////////////////////
+//                     Para la version con piezo                      //
+////////////////////////////////////////////////////////////////////////
+/*
+ if (CO2 < 250)
   {
-    tone(5,CO2,400); // output pin (PWM), variable, duration of the pulse
+    tone(5,CO2,400); // sortida (PWM), variable, duración del 'pulse'
     delay(100);
     noTone(5);
-    delay(1000);// do Thing A
+    delay(1000);
     }
    else if (CO2 >= 250 && CO2 < 666)
     {
       for (int i=0;i<2;i++)
         {
-        tone(5,CO2,400); // output pin (PWM), variable, duration of the pulse
+        tone(5,CO2,400); 
         delay(100);
         noTone(5);
-        delay(300);// do Thing B
+        delay(300);
         }
         delay(200);
     }
@@ -36,27 +51,20 @@ if (CO2 < 250)
     {
     for (int i=0;i<3;i++)
         {
-        tone(5,CO2,400); // output pin (PWM), variable, duration of the pulse
+        tone(5,CO2,400);
         delay(75);
         noTone(5);
         delay(100);
               } 
               delay(400);
-      }
-else
-{
-    tone(5,CO2,400); // output pin (PWM), variable, duration of the pulse
+        }
+  else
+    {
+    tone(5,CO2,400); 
     delay(75);
     noTone(5);
-    delay(75);// do Thing C
+    delay(75);
 }
-
-/////////////////////// Para la version Android ///////////////////////////////
-/*
-tone(5,CO2,400); // output pin (PWM), variable, duration of the pulse
-delay(100);
-noTone(5);
-delay(500);
 */
 
 }
