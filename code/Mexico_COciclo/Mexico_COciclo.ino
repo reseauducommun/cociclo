d97c24592fe19854019ead4ecbc63294a06c83c0
Alexandre Castonguay <acastonguay@artengine.ca>
2015-09-24 11:13:05 -0500

    archive pour android México et petite correction de commentaire

--- code/Mexico_COciclo/Mexico_COciclo.ino
+++ code/Mexico_COciclo/Mexico_COciclo.ino
@@ -8,7 +8,7 @@ Serial.begin(9600);
 
 void loop() {
 
-CO = (analogRead(A7)); leer el valor del sensor
+CO = (analogRead(A7)); //leer el valor del sensor
 Serial.print("CO : ");
 Serial.println(CO);
 
@@ -20,23 +20,23 @@ Serial.println();
 ////////////////////////////////////////////////////////////////////////
 //                     Para la version Android                        //
 ////////////////////////////////////////////////////////////////////////
-
+/*
 tone(5,CO2,400); // sortida (PWM 5 en este caso), variable, duración del pulse
-delay(100);
+//delay(100);
 
-
+*/
 ////////////////////////////////////////////////////////////////////////
 //                     Para la version con piezo                      //
 ////////////////////////////////////////////////////////////////////////
-/*
- if (CO2 < 250)
+
+ if (CO2 < 170)
   {
     tone(5,CO2,400); // sortida (PWM), variable, duración del 'pulse'
     delay(100);
     noTone(5);
     delay(1000);
     }
-   else if (CO2 >= 250 && CO2 < 666)
+   else if (CO2 >= 170 && CO2 < 366)
     {
       for (int i=0;i<2;i++)
         {
@@ -47,24 +47,24 @@ delay(100);
         }
         delay(200);
     }
-    else if (CO2 >= 666 && CO2 < 900)
+    else if (CO2 >= 366 && CO2 < 666 )
     {
     for (int i=0;i<3;i++)
         {
         tone(5,CO2,400);
-        delay(75);
+        delay(65);
         noTone(5);
-        delay(100);
+        delay(80);
               } 
               delay(400);
         }
   else
     {
-    tone(5,CO2,400); 
-    delay(75);
-    noTone(5);
-    delay(75);
+   tone(5,CO2,400); 
+   delay(75);
+  noTone(5);
+   delay(75);
 }
-*/
+
 
 }
