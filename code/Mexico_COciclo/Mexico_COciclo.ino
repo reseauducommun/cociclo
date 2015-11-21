0b6546609db25ba348e23cbc7355790e5fb5dfaf
Alexandre Castonguay <acastonguay@artengine.ca>
2015-11-21 15:03:26 +0100

    Piksel Bergen Version
    Switching to UDP over wifi and a design with the huzzah.

--- code/Mexico_COciclo/Mexico_COciclo.ino
+++ code/Mexico_COciclo/Mexico_COciclo.ino
@@ -1,5 +1,5 @@
 int CO = 0;
-int CO2 = 0;
+int BESTIA = 0;
 
 void setup() {
 
@@ -10,48 +10,46 @@ void loop() {
 
 CO = (analogRead(A7)); //leer el valor del sensor
 Serial.print("CO : ");
-Serial.println(CO);
+//Serial.println(CO);
 
-CO2 = map(CO,80, 400, 150,666); // sustituir los valores en un nuevo intervalo para escuchar
-Serial.print("CO2 : ");
-Serial.println(CO2); //nuevo, 'CO2' es une chistosa
-Serial.println();
+BESTIA = map(CO,80, 400, 150,666); // sustituir los valores en un nuevo intervalo para escuchar
+//Serial.print("CO2 : ");
+//Serial.println(CO2); //nuevo, 'CO2' es une chistosa
+//Serial.println();
 
 ////////////////////////////////////////////////////////////////////////
 //                     Para la version Android                        //
 ////////////////////////////////////////////////////////////////////////
 /*
-tone(5,CO2,400); // sortida (PWM 5 en este caso), variable, duración del pulse
+tone(5,BESTIA,400); // sortida (PWM 5 en este caso), variable, duración del pulse
 //delay(100);
-
 */
 ////////////////////////////////////////////////////////////////////////
 //                     Para la version con piezo                      //
 ////////////////////////////////////////////////////////////////////////
-
- if (CO2 < 170)
+ if (BESTIA < 170)
   {
-    tone(5,CO2,400); // sortida (PWM), variable, duración del 'pulse'
-    delay(100);
+    tone(5,BESTIA,400); // sortida (PWM), variable, duración del 'pulse'
+    delay(50);
     noTone(5);
     delay(1000);
     }
-   else if (CO2 >= 170 && CO2 < 366)
+   else if (BESTIA >= 170 && BESTIA < 366)
     {
       for (int i=0;i<2;i++)
         {
-        tone(5,CO2,400); 
+        tone(5,BESTIA,400); 
         delay(100);
         noTone(5);
         delay(300);
         }
         delay(200);
     }
-    else if (CO2 >= 366 && CO2 < 666 )
+    else if (BESTIA >= 366 && BESTIA < 666 )
     {
     for (int i=0;i<3;i++)
         {
-        tone(5,CO2,400);
+        tone(5,BESTIA,400);
         delay(65);
         noTone(5);
         delay(80);
@@ -60,7 +58,7 @@ tone(5,CO2,400); // sortida (PWM 5 en este caso), variable, duración del pulse
         }
   else
     {
-   tone(5,CO2,400); 
+   tone(5,BESTIA,400); 
    delay(75);
   noTone(5);
    delay(75);
