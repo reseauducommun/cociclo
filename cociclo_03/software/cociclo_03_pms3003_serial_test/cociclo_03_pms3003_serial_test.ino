6d49c75625539b5b8d3128dabaedf0de61920224
Alexandre Castonguay <acastonguay@artengine.ca>
2018-01-28 22:17:28 -0500

    PMS3003 works!
    Next step is to integrate everything in the main code

---
+++ cociclo_03/software/cociclo_03_pms3003_serial_test/cociclo_03_pms3003_serial_test.ino
@@ -0,0 +1,117 @@
+//******************************
+ //*Abstract: Read value of PM1,PM2.5 and PM10 of air quality
+ //*Product Link: http://www.dfrobot.com.cn/goods-1113.html
+ //*
+ //*
+ //*The RX pin on the sensor connects to pin D5 on the Wemnos D1 mini
+ //*The TX pin on the sensor connects to pin D6 on the Wemnos D1 mini
+ //
+ //*Version：V3.1
+ //*Author：Zuyang @ HUST
+ //*Date：March.25.2016
+ //******************************
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+#define LENG 31   //0x42 + 31 bytes equal to 32 bytes
+//#define LENG 23   //0x42 + 23 bytes equal to 24 bytes
+unsigned char buf[LENG];
+
+int PM01Value=42;          //define PM1.0 value of the air detector module
+int PM2_5Value=42;         //define PM2.5 value of the air detector module
+int PM10Value=42;         //define PM10 value of the air detector module
+
+SoftwareSerial PMSerial(D5, D6); // RX, TX
+
+void setup()
+{
+  pinMode(D3,OUTPUT);
+  PMSerial.begin(9600);   
+  PMSerial.setTimeout(1500);    
+  Serial.begin(115200);
+}
+
+void loop()
+{
+
+ digitalWrite(D3,HIGH); // to put it in constant operation
+
+  
+  if(PMSerial.find(0x42)){    
+  
+     PMSerial.readBytes(buf,LENG);
+     // Serial.println(PMSerial.readBytes(buf,LENG));
+     // Serial.println(buf[0]); 0x4d
+    
+ 
+    if(buf[0] == 0x4d){
+     // if(checkValue(buf,LENG)){ // ne fonctionne pas
+        PM01Value=transmitPM01(buf); //count PM1.0 value of the air detector module
+        PM2_5Value=transmitPM2_5(buf);//count PM2.5 value of the air detector module
+        PM10Value=transmitPM10(buf); //count PM10 value of the air detector module 
+     // }           
+    } 
+  }
+
+  static unsigned long OledTimer=millis();  
+    if (millis() - OledTimer >=1000) 
+    {
+      OledTimer=millis(); 
+      
+      Serial.print("PM1.0: ");  
+      Serial.print(PM01Value);
+      Serial.println("  ug/m3");            
+    
+      Serial.print("PM2.5: ");  
+      Serial.print(PM2_5Value);
+      Serial.println("  ug/m3");     
+      
+      Serial.print("PM1 0: ");  
+      Serial.print(PM10Value);
+      Serial.println("  ug/m3");   
+      Serial.println();
+    }
+  
+}
+char checkValue(unsigned char *thebuf, char leng)
+{  
+  char receiveflag=0;
+  int receiveSum=0;
+
+  for(int i=0; i<(leng-2); i++){
+  receiveSum=receiveSum+thebuf[i];
+  }
+  receiveSum=receiveSum + 0x42;
+ 
+  if(receiveSum == ((thebuf[leng-2]<<8)+thebuf[leng-1]))  //check the serial data 
+  {
+    receiveSum = 0;
+    receiveflag = 1;
+  }
+  return receiveflag;
+}
+
+int transmitPM01(unsigned char *thebuf)
+{
+  int PM01Val;
+  PM01Val=((thebuf[3]<<8) + thebuf[4]); //count PM1.0 value of the air detector module
+  return PM01Val;
+}
+
+//transmit PM Value to PC
+int transmitPM2_5(unsigned char *thebuf)
+{
+  int PM2_5Val;
+  PM2_5Val=((thebuf[5]<<8) + thebuf[6]);//count PM2.5 value of the air detector module
+  return PM2_5Val;
+  }
+
+//transmit PM Value to PC
+int transmitPM10(unsigned char *thebuf)
+{
+  int PM10Val;
+  PM10Val=((thebuf[7]<<8) + thebuf[8]); //count PM10 value of the air detector module  
+  return PM10Val;
+}
+
+
